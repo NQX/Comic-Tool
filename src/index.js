77a26ea051cfada87cf660c5824feb721e066533
Tobias <nqxdev@gmail.com>
2020-06-28 13:56:07 +0200

    cbz working

--- src/index.js
+++ src/index.js
@@ -16,18 +16,23 @@ var unrar = require("node-unrar");
 var zip = require('extract-zip');
 var zipFolder = require('zip-a-folder');
 var rimraf = require('rimraf');
-var temp = require('temp');
 
-var nrc = require('node-run-cmd');
+
+var temp = require('temp').track();
+
+
 
 var originalFilename = '';
 
 var mainWindow;
-var qualtiy = 80;
+var qualtiy;
 var filesArray = [];
 
 var fileCounter = 0;
 var totalFilesCount = 0;
+
+
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -48,7 +53,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   createFolders();
 };
@@ -89,6 +94,7 @@ ipcMain.on('ondragstart', (event, filePath) => {
 
 
 ipcMain.on('start-convert', (event, arg) => {
+  console.log('start qual', arg.quality)
   quality = arg.quality
 
 
@@ -157,15 +163,15 @@ const getFileFromUser = async () => {
 
 
 function createFolders() {
-  fs.exists(__dirname + '/tmp', (exists) => {
+  fs.exists(app.getPath('documents') + '/tmp', (exists) => {
     if (!exists) {
-      fs.mkdirSync(__dirname + '/tmp', () => {});
+      fs.mkdirSync(app.getPath('documents') + '/tmp', () => {});
     }
   });
 
-  fs.exists(__dirname + '/webp', (exists) => {
+  fs.exists(app.getPath('documents') + '/webp', (exists) => {
     if (!exists) {
-      fs.mkdirSync(__dirname + '/webp', () => {});
+      fs.mkdirSync(app.getPath('documents') + '/webp', () => {});
     }
   });
 
@@ -175,14 +181,16 @@ function createFolders() {
 
 
 function convertBatch(file) {
-  let dir = __dirname + "/tmp/"
+  let dir = app.getPath('documents') + "/tmp/"
+
+  console.log(dir)
 
   fs.readdir(dir, (err, files) => {
     
     if(hasEmptySpaces(files[0])) {
       //console.log('needs rename', file.path, files[0])
-      console.log('rename', file.path + "tmp/" + files[0], file.path + "tmp/" + removeSpaces(files[0]))
-      fs.rename(file.path + "tmp/" + files[0], file.path + "tmp/" + removeSpaces(files[0]), function(err) {
+      
+      fs.rename(dir  + files[0], dir  + removeSpaces(files[0]), function(err) {
         if(err) {
           console.log(err)
           return;
@@ -210,8 +218,6 @@ function convertBatch(file) {
 
     hasEmptySpaces(dir, files[0], function () {
 
-
-      
 
 
     })
@@ -247,7 +253,6 @@ function hasEmptySpaces(dir, files, cb) {
 */
 
 function convertComic(filePath) {
-
   
   let i = 0;
 
@@ -272,7 +277,7 @@ convert(filePath);
 
 function convert(filePath) {
   
-  let dir = __dirname + "/tmp/"
+  let dir = app.getPath('documents') + "/tmp/"
   
   i = 0;
 
@@ -345,7 +350,7 @@ async function unzip(file) {
 
   try {
     await zip(path.resolve(file.fullpath), {
-      dir: __dirname + "/tmp/"
+      dir: app.getPath('documents') + "/tmp/"
     },)
     console.log('Extraction complete')
     convertBatch(file)
@@ -357,7 +362,7 @@ async function unzip(file) {
 }
 
 
-let outputDir = __dirname + "/webp/"
+let outputDir = app.getPath('documents') + "/webp/"
 
 
 
@@ -376,12 +381,12 @@ function unrarFile(file) {
 
   
 
-  fs.mkdir(__dirname + '/tmp/' + newDirectoryName, function(err) {
+  fs.mkdir(app.getPath('documents') + '/tmp/' + newDirectoryName, function(err) {
     if(err) console.log(err)
 
 
     
-    rar.extract(file.path + "tmp/" + newDirectoryName, null, function(err) {
+    rar.extract(app.getPath('documents') + "/tmp/" + newDirectoryName, null, function(err) {
       if(err) {
         console.log(err)
         return;
@@ -411,7 +416,7 @@ function unrarFile(file) {
 function convertToWebp(inputPath, fileName) {
   console.log(' output dir',inputPath, fileName)
 
-  
+  console.log('quality', qualtiy)
   
   webp.cwebp(inputPath + '/' + fileName, outputDir + removeFileEnd(fileName) + ".webp", "-q "+quality, function (status, error) {
     //if conversion successful status will be '100'
@@ -430,11 +435,12 @@ function convertToWebp(inputPath, fileName) {
 
 
 function  packFiles() {
-    zipFolder.zipFolder(__dirname  + '/webp', __dirname + "/out.cbz", function(err) {
+    zipFolder.zipFolder(app.getPath('documents')  + '/webp', app.getPath('downloads') + "/out.cbz", function(err) {
     if(err) {
       console.log('oh no!', err);
   } else {
       console.log('EXCELLENT');
+      const response = dialog.showMessageBox(null, {message: 'Done!'});
       deleteFolders();
   }
   })
@@ -442,11 +448,11 @@ function  packFiles() {
 
 
 function deleteFolders() {
-  rimraf(__dirname + '/webp',() => {
+  rimraf(app.getPath('documents') + '/webp',() => {
     console.log('done')
   });
 
-  rimraf(__dirname + '/tmp',() => {
+  rimraf(app.getPath('documents') + '/tmp',() => {
     console.log('done 2')
   })
 }
